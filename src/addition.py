cd423315d53962cfeaf23ccf03e96454b56a24ad
Vamshi Nalla <nvkrishna9396@gmail.com>
2024-11-25 22:12:59 +0530

    New function added

--- src/addition.py
+++ src/addition.py
@@ -12,4 +12,15 @@ def sub(a,b):
 
 def test_sub():
     assert sub(2,1) == 1
-    assert sub(10,5) == 5
+    assert sub(10,5) == 5 
+
+
+def division(a,b):
+    if a>0:
+        return a//b
+    else:
+        return False
+    
+def test_division():
+    assert division(10,5) == 2
+    assert division(-10,5) == False
